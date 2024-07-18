76c0504a99e3d58f6dffe39b0dbd3def0872d996
Gustavo Sforça <gustavo_sforca@hotmail.com>
2024-07-18 13:19:15 -0300

    Portifolio

---
+++ assets/js/habilidades.js
@@ -0,0 +1,38 @@
+const skills = document.querySelectorAll('.skill-box');
+const descricao = document.querySelector('.texto-descricao');
+const sobreSkill = [
+                    '<p>HTML</p> <br> <p>É uma linguagem de marcação utilizada na construção de páginas na Web.</p> <br>',
+                    '<p>CSS</p> <br> <p>É um mecanismo para adicionar estilo a um documento web.</p> <br>',
+                    '<p>JavaScript</p> <br> <p>É uma linguagem de programação. Juntamente com HTML e CSS, é uma das três principais tecnologias da web.</p> <br>',
+                    '<p>Next.JS</p> <br> <p>O Next.js é um framework React que permite construir sites e aplicativos web estáticos otimizados, altamente interativos e focados na experiência do usuário.</p> <br>',
+                    '<p>SQL</p> <br> <p>É uma linguagem para trabalhar com bancos de dados relacionais, amplamente utilizada por profissionais em diversas áreas.</p> <br>',
+                    '<p>Segurança da informação</p> <br> <p>É à defesa de dados e à prática que assegura que informações sigilosas possam ser acessadas somente por aqueles a quem estas se referem.</p> <br>',
+                    '<p>Git</p> <br> <p>É um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software.</p> <br>',
+                    '<p>Java</p> <br> <p>É uma linguagem de programação orientada a objetos. </p> <br>'
+]
+
+const dataInicio = [
+                    (new Date(2021, 10, 1)).getTime(), // 0
+                    (new Date(2021, 10, 1)).getTime(), // 1
+                    (new Date(2022, 0, 1)).getTime(), // 2
+                    (new Date(2022, 3, 1)).getTime(), // 3
+                    (new Date(2022, 3, 1)).getTime(), // 4
+                    (new Date(2022, 1, 1)).getTime(), // 5
+                    (new Date(2021, 11, 1)).getTime(), // 6
+                    (new Date(2021, 11, 1)).getTime(), // 7
+                    ]
+const dataHoje = (new Date()).getTime();
+
+skills.forEach(  (elemento, index) => {
+    let index1 = index;
+    let elemento1 = elemento;
+    elemento.addEventListener('mouseover', (evento) => {
+        let tempo = 'mês';
+        let tempoDeExperiencia = Math.round((dataHoje-dataInicio[index1])/(1000*60*60*24*30));
+        if (tempoDeExperiencia > 1) tempo = 'meses'
+        descricao.innerHTML = `<p>${sobreSkill[index1]} </p>` ;
+    } )
+    elemento.addEventListener('mouseout', (evento, elemento,) => {
+        descricao.innerHTML = '/* Passe o mouse por cima de alguma habilidade para ler a descrição */';
+    } )
+} );
